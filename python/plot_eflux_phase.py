4dc03b235f9a15d51fca2a71fbaca2ec12e094b4
richardxdubois <dubois@stanford.edu>
2024-11-20 18:11:20 -0800

    rewrite for super+orbital phase matrix

--- python/plot_eflux_phase.py
+++ python/plot_eflux_phase.py
@@ -18,57 +18,99 @@ parser.add_argument('--app_config',
 
 args = parser.parse_args()
 
-with open(args.app_config, "r") as f:
-    data = yaml.safe_load(f)
+class plot_eflux_phase():
 
-source_name = data["source"]
-fgl_source = data["4FGL_source"]
+    def __init__(self, app_config):
 
-num_pickles = data["num_pickles"]
-p_bins = np.arange(num_pickles)
-html = data["html"]
-page_title = data["page_title"]
 
-print(num_pickles, p_bins)
+        with open(app_config, "r") as f:
+            data = yaml.safe_load(f)
 
-base_fn = data["base_fn"]
-sed_prefix = data["sed_prefix"]
+        self.source_name = data["source"]
+        self.fgl_source = data["4FGL_source"]
 
-seds = []
+        self.num_pickles = data["num_pickles"]
+        self.p_bins = np.arange(self.num_pickles)
+        self.html = data["html"]
+        self.page_title = data["page_title"]
 
-for phase_bin in np.arange(num_pickles):
+        self.base_fn = data["base_fn"]
+        self.sed_prefix = data["sed_prefix"]
 
-    if sed_prefix == "":
-        infile = base_fn + str(phase_bin) + "/" + fgl_source + "_sed.npy"
-    else:
-        infile = base_fn + str(phase_bin) + "/" + sed_prefix + str(phase_bin) + "_" + fgl_source + "_sed.npy"
+        self.seds = {}
+        self.eflux = None
+        self.eflux_err_lo = None
+        self.eflux_err_hi = None
+        self.loge_ctr = None
+        self.b_lower = None
+        self.b_upper = None
 
-    p = np.load(infile, allow_pickle=True).flat[0]
+        self.num_pickles_2 = 0
+        self.base_fn_2 = None
+        self.p_bins_2 = []
+        try:
+            self.num_pickles_2 = data["num_pickles_2"]
+            self.base_fn_2 = data["base_fn_2"]
+            self.p_bins_2 = np.arange(self.num_pickles_2)
+        except KeyError:
+            pass
 
-    eflux = p["e2dnde"]
-    eflux_err_lo = p["e2dnde_err_lo"]
-    eflux_err_hi = p["e2dnde_err_hi"]
-    loge_ctr = np.power(10, p["loge_ctr"])
+    def loop_over_bins(self):
 
-    b_upper = [x+e for x, e in zip(eflux, eflux_err_hi)]
-    b_lower = [x-e for x, e in zip(eflux, eflux_err_lo)]
+        for phase_bin in self.p_bins:
 
-    source = ColumnDataSource(data=dict(x=loge_ctr, y=eflux, upper=b_upper, lower=b_lower))
+            self.seds[phase_bin] = []
 
-    # Log-log plot
-    p_fig = figure(x_axis_type="log", y_axis_type="log",
-                   title="Phase bin " + str(phase_bin) + ": E^2 dN/dE vs Energy", x_axis_label='E (MeV)',
-                   y_axis_label='E^2 dN/dE', tooltips=[('Eflux', '@y'), ('Energy', '@x')], x_range=(100, 100000),
-                   y_range=(5e-7, 5.e-4))
+            for phase_bin_2 in self.p_bins_2:
 
-    p_fig.line(source=source, x="x", y="y", legend_label="E^2 dN/dE", line_width=2)
-    p_fig.scatter(loge_ctr, eflux, size=8, fill_color="white")
-    p_fig.add_layout(Whisker(source=source, base="x", upper="upper", lower="lower", level="overlay"))
+                if self.num_pickles_2 == 0:
+                    infile = self.base_fn + str(phase_bin) + "/" + self.sed_prefix + \
+                              str(phase_bin) + "_" + self.fgl_source + "_sed.npy"
+                else:
+                    infile = (self.base_fn + str(phase_bin) + "/" + self.base_fn_2 +
+                        str(phase_bin_2) + self.sed_prefix + str(phase_bin_2) + "_" +
+                              self.fgl_source + "_sed.npy")
 
-    seds.append(p_fig)
+                p = np.load(infile, allow_pickle=True).flat[0]
 
-del_div = Div(text=source_name + " Run on: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                self.eflux = p["e2dnde"]
+                self.eflux_err_lo = p["e2dnde_err_lo"]
+                self.eflux_err_hi = p["e2dnde_err_hi"]
+                self.loge_ctr = np.power(10, p["loge_ctr"])
 
-output_file(html)
-l = layout(del_div, column(seds))
-save(l, title=page_title)
+                self.b_upper = [x+e for x, e in zip(self.eflux, self.eflux_err_hi)]
+                self.b_lower = [x-e for x, e in zip(self.eflux, self.eflux_err_lo)]
+
+                rc = self.make_plot(phase_bin1=phase_bin, phase_bin2=phase_bin_2)
+
+    def make_plot(self, phase_bin1, phase_bin2=None):
+
+        source = ColumnDataSource(data=dict(x=self.loge_ctr, y=self.eflux, upper=self.b_upper,
+                                            lower=self.b_lower))
+
+        title = "Phase bin " + str(phase_bin1)
+        if phase_bin2 is not None:
+            title += " , Phase bin 2 " + str(phase_bin2)
+        title += " : E^2 dN/dE vs Energy"
+
+        # Log-log plot
+        p_fig = figure(x_axis_type="log", y_axis_type="log",
+                       title=title, x_axis_label='E (MeV)',
+                       y_axis_label='E^2 dN/dE', tooltips=[('Eflux', '@y'), ('Energy', '@x')], x_range=(100, 100000),
+                       y_range=(5e-7, 5.e-4))
+
+        p_fig.line(source=source, x="x", y="y", legend_label="E^2 dN/dE", line_width=2)
+        p_fig.scatter(self.loge_ctr, self.eflux, size=8, fill_color="white")
+        p_fig.add_layout(Whisker(source=source, base="x", upper="upper",
+                                 lower="lower", level="overlay"))
+
+        self.seds[phase_bin1].append(p_fig)
+
+    def output_plot(self):
+
+        del_div = Div(text=self.source_name + " Run on: " +
+                           datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+        output_file(self.html)
+        l = layout(del_div, column(self.seds))
+        save(l, title=self.page_title)
