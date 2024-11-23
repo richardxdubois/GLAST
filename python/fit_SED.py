6d960dec96cd81667019d8da533265d6cf44094e
Richard Dubois <richard.x.dubois@gmail.com>
2024-11-23 11:44:07 -0800

    try float64s

--- python/fit_SED.py
+++ python/fit_SED.py
@@ -3,6 +3,12 @@ from scipy.optimize import curve_fit
 
 
 def SED_function(E, A, alpha, E_cut, E_0):
+    E = np.float64(E)
+    A = np.float64(A)
+    alpha = np.float64(alpha)
+    E_cut = np.float64(E_cut)
+    E_0 = np.float64(E_0)
+
     return E*E*A * (E / E_0)**(-alpha) * np.exp(-E / E_cut)
 
 
