7f32de93b8db8a87033db819f1c493d8e2c26eb9
Richard Dubois <richard.x.dubois@gmail.com>
2024-11-23 18:38:35 -0800

    fiddling with fit model

--- python/fit_SED.py
+++ python/fit_SED.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
-def SED_function(E, A, alpha, E_cut, E_0):
+def SED_function(E, A, alpha, E_cut):
     E = np.float64(E)
     A = np.float64(A)
     alpha = np.float64(alpha)
     E_cut = np.float64(E_cut)
-    E_0 = np.float64(E_0)
+    #E_0 = np.float64(E_0)
 
-    return E*E*A * (E / E_0)**(-alpha) * np.exp(-E / E_cut)
+    return E*E*A * (E / 1800.)**(-alpha) * np.exp(-E / E_cut)
 
 
 def fit_SED(E, flux, errors, initial_guesses):
@@ -21,11 +21,7 @@ def fit_SED(E, flux, errors, initial_guesses):
         flux,
         p0=initial_guesses,
         sigma=errors,
-        absolute_sigma=True, method="trf"
-    )
-
-
-    # Extract fitted parameters
-    A, alpha, E_cut, E_0 = params
+        absolute_sigma=True, method="trf", bounds=(0, [1e-4, 3., 8000.])
+        )
 
     return params, covariance
