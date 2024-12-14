f9f0528aff8150a786a0c61840e9dae7f7275275
Richard Dubois <richard.x.dubois@gmail.com>
2024-12-14 12:07:19 -0800

    test turning off gta.optimize

--- python/lsi_phased.py
+++ python/lsi_phased.py
@@ -24,7 +24,7 @@ gta = GTAnalysis(args.config, logging={'verbosity': 3})
 print(args.overwrite)
 gta.setup(overwrite=args.overwrite)
 
-gta.optimize()
+#gta.optimize()
 
 model = gta.roi.get_source_by_name(args.source)
 print(model)
