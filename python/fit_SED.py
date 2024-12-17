70c78c5dcd6e2d99f8695efa5e9ec34fe05153d1
Richard Dubois <richard.x.dubois@gmail.com>
2024-12-17 11:58:41 -0800

    add PL functions

--- python/fit_SED.py
+++ python/fit_SED.py
@@ -2,39 +2,62 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.integrate import quad
 
+
 def SED_function(E, A, alpha, E_cut):
     E = np.float64(E)
     A = np.float64(A)
     alpha = np.float64(alpha)
     E_cut = np.float64(E_cut)
-    #E_0 = np.float64(E_0)
 
     return E*E*A * (E / 1800.)**(-alpha) * np.exp(-E / E_cut)
+
 
 def flux_function(E, A, alpha, E_cut):
     E = np.float64(E)
     A = np.float64(A)
     alpha = np.float64(alpha)
     E_cut = np.float64(E_cut)
-    #E_0 = np.float64(E_0)
+
     return A * (E/1800.)**(-alpha) * np.exp(-E/E_cut)
 
-def fit_SED(E, flux, errors, initial_guesses):
+
+def fit_SED(func, E, flux, errors, initial_guesses):
+
+    if len(initial_guesses) > 2:
+        bounds = [1e-4, 3., 15000.]
+    else:
+        bounds = [1e-4, 3.]
 
     # Fit the model
     params, covariance = curve_fit(
-        SED_function,
+        func,
         E,
         flux,
         p0=initial_guesses,
         sigma=errors,
-        absolute_sigma=True, method="trf", bounds=(0, [1e-4, 3., 15000.])
+        absolute_sigma=True, method="trf", bounds=(0, bounds)
         )
 
     return params, covariance
 
 
-def fit_SED_errors(E_min, E_max, params, covariance):
+def SED_PL_function(E, A, alpha):
+    E = np.float64(E)
+    A = np.float64(A)
+    alpha = np.float64(alpha)
+
+    return E*E*A * (E / 1800.)**(-alpha)
+
+
+def flux_PL_function(E, A, alpha):
+    E = np.float64(E)
+    A = np.float64(A)
+    alpha = np.float64(alpha)
+
+    return A * (E/1800.)**(-alpha)
+
+
+def fit_SED_errors(flux_function, E_min, E_max, params, covariance):
 
     i_params = []
     integral_diffs = []
@@ -43,11 +66,14 @@ def fit_SED_errors(E_min, E_max, params, covariance):
 
         cov = covariance[p][p]
         err = []
-        for i in range(-1,2):
+        for i in range(-1, 2):
             i_params = list(params)
             i_params[p] += i*np.sqrt(cov)
-            integrated_fits, int_error = quad(flux_function, E_min, E_max,
-                                              args=(i_params[0], i_params[1], i_params[2]))
+            if len(i_params) > 2:
+                args = (i_params[0], i_params[1], i_params[2])
+            else:
+                args = (i_params[0], i_params[1])
+            integrated_fits, int_error = quad(flux_function, E_min, E_max, args=args)
             err.append(integrated_fits)
         integral_diffs.append(abs(err[1]-err[0]))
         integral_diffs.append(abs(err[2]-err[1]))
