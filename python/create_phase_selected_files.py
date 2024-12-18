b9cb6e22ad2e51f1734b242697ec9b1739a2e07e
Richard Dubois <richard.x.dubois@gmail.com>
2024-12-18 09:15:51 -0800

    update for t0 in manual ftcopy

--- python/create_phase_selected_files.py
+++ python/create_phase_selected_files.py
@@ -1,37 +1,38 @@
 # read list of input files and loop over them with ftcopy to apply a filter selecting a phase bin
 import numpy as np
 import os
+import yaml
+import argparse
 
-source = "LSI61303"
-period = "26.4960"
-file_list = "/Users/richarddubois/Code/GLAST/tmp/binned_events.txt"
-n_phase_bins = 10
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--app_config',
+                    default="/Users/richarddubois/Code/GLAST/tmp/dbg/phase_config.yaml",
+                    help="overall app config file")
+args = parser.parse_args()
+
+with open(args.app_config, "r") as f:
+    data = yaml.safe_load(f)
+
+source = data["source"]
+
+n_phase_bins = data["n_phase_bins"]
+phase_bin = data["phase_bin"]
+t0 = data["t0"]
+
+period = data["period"]
+
+infile = data["infile"]
+
 delta = 1./n_phase_bins
 filter_string = []
 
-f = open(file_list, "r")
-flist = []
-for input in f:
-    flist.append(input.rstrip())
-
-for p in np.arange(n_phase_bins):
-    g_name = source + "_phase_" + str(p) + ".txt"
-    g = open(g_name, "w")
-    i = 0
-    for infile in flist:
-        t1 = p/n_phase_bins
-        t2 = str(t1 + delta)
-        t1 = str(t1)
-        o_file = source + "_file_" + str(i) + "_phase_" + str(p) + ".fits"
-        f_string = "ftcopy '" + infile + "[EVENTS]" \
-                    "[((((TIME/86400)%" + period + ")/" + period + " >=" + t1 + ") .and. " \
-                    "(((TIME/86400.)%" + period + ")/" + period + "<" + t2 + "))]' outfile=" + o_file
-        filter_string.append(f_string)
-        res = os.system(f_string)
-        g.write(o_file + "\n")
-        i += 1
-    g.close()
-
-
-
-print(len(filter_string))
+t1 = phase_bin/n_phase_bins
+t2 = str(t1 + delta)
+t1 = str(t1)
+o_file = "ft1_phase.fits"
+f_string = "ftcopy '" + infile + "[EVENTS]" \
+            "[(((((TIME-" + str(t0) + ")/86400)%" + period + ")/" + period + " >=" + t1 + ") .and. " \
+            "((((TIME-" + str(t0) + ")/86400.)%" + period + ")/" + period + "<" + t2 + "))]' outfile=" + o_file
+print(f_string)
+res = os.system(f_string)
