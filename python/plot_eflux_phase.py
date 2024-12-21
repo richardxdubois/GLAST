de2490c047b6cc9b76fe931226fa036dee1e42b2
Richard Dubois <richard.x.dubois@gmail.com>
2024-12-21 14:04:54 -0800

    add contour map of flux

--- python/plot_eflux_phase.py
+++ python/plot_eflux_phase.py
@@ -402,7 +402,7 @@ class plot_eflux_phase():
         p_cont = figure(title="Contour Map Flux", width=600, height=600, x_axis_label='orbital', y_axis_label='super')
 
         # Create a color mapper for the data
-        cont_mapper = linear_cmap('value', palette=Viridis256, low=np.array(self.fermipy_flux).min(),
+        cont_mapper = linear_cmap('value', palette=palette, low=np.array(self.fermipy_flux).min(),
                                   high=np.array(self.fermipy_flux).max())
 
         r = p_cont.rect(x="x", y="y", width=1, height=1, source=source,
@@ -420,7 +420,14 @@ class plot_eflux_phase():
                            line_width=1)
 
         # Add color bar
-        #p_cont.add_layout(r.construct_color_bar, 'right')
+        p_cont.add_layout(r.construct_color_bar(
+            major_label_text_font_size="12px",
+            ticker=BasicTicker(desired_num_ticks=len(colors)),
+
+            label_standoff=6,
+            border_line_color=None,
+            padding=5,
+        ), 'right')
 
         # create sliders
         steps = (high - low)/20.
