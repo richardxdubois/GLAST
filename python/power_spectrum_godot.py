ad7cb980250a628409f0058acf138bd88a0c5a69
Richard Dubois <richard.x.dubois@gmail.com>
2025-01-20 09:50:46 -0800

    starter version

---
+++ python/power_spectrum_godot.py
@@ -0,0 +1,40 @@
+import pylab as pl
+import numpy as np
+from scipy.stats import chi2,norm
+
+import core, load_data
+from load_data import data_path, get_data
+
+
+ft1 = "/sdf/home/r/richard/fermi-user/LSI61303/periods/periodicity/LSI61303_1_deg_mkt_500s.fits"
+ft2 = "/sdf/home/r/richard/fermi-user/LSI61303/fssc_data/L24082417075904476C3F57_SC00.fits"
+
+ra = 40.143
+dec = 61.229
+spectrum = lambda E: (E/1000)**-2.1
+
+data = core.Data(ft1, ft2, ra, dec, "LSI61303", base_spectrum=spectrum,zenith_cut=90)
+
+print("done so far")
+
+"""
+ts = data.get_cells(tcell=300,time_series_only=True,
+            trim_zero_exposure=False,use_barycenter=True)
+    f,window = core.power_spectrum_fft(ts,exp_only=True)
+    scale = 50./40000
+    f,dlogl_nobg,dlogl,dlogl_null = core.power_spectrum_fft(ts)
+    fday = f*86400
+
+    forb = 2.963145573933919e-06
+    fprec = 2.1777777777777778e-07
+    freqs = np.asarray([fprec,forb,2*forb])
+    corr,pows = core.get_orbital_modulation(ts,freqs)
+    f2,dlogl_nobg2,dlogl2,dlogl_null2 = core.power_spectrum_fft(ts,
+            exposure_correction=corr)
+
+    add_power = np.zeros_like(dlogl_nobg2)
+    for freq,p in zip(freqs,pows):
+        idx = np.argmin(np.abs(f[1:]-freq))
+        add_power[idx] = p
+
+"""
