1dc0cea37ec192ac3dfbe742e1ed2e7444549570
Richard Dubois <richard.x.dubois@gmail.com>
2025-01-26 09:13:58 -0800

    add plots

--- python/power_spectrum_godot.py
+++ python/power_spectrum_godot.py
@@ -56,7 +56,7 @@ f1 = figure(title="frequency", y_axis_type="log", width=800, height=640)
 f1.line(fday[fmask], dlogl_nobg[fmask])
 
 pday = [1./f for f in fday if f != 0]
-pmask = np.where((pday > 23.) & (pday < 29.))
+pmask = [i for i in range(len(pday)) if pday[i] > 23 and pday[i] < 29]
 
 f1 = figure(title="period", y_axis_type="log", width=800, height=640)
 f2.line(pday[pmask], dlogl[pmask])
