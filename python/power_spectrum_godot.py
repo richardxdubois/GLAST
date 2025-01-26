5a19df0ac940a06f02137a05b451eb2c4bceb13e
Richard Dubois <richard.x.dubois@gmail.com>
2025-01-25 19:17:41 -0800

    fix ft1 spec

--- python/power_spectrum_godot.py
+++ python/power_spectrum_godot.py
@@ -5,6 +5,14 @@ import os
 
 from godot import core
 
+from bokeh.plotting import figure, output_file, reset_output, show, save
+from bokeh.layouts import row, layout, column, gridplot
+from bokeh.models import (Label, Span, LinearAxis, Range1d, Whisker, ColumnDataSource, BasicTicker, Tabs,
+                          TabPanel, RangeSlider, CustomJS, Button, NumeralTickFormatter, BasicTickFormatter)
+from bokeh.models.widgets import Div
+from bokeh.palettes import Plasma256 as palette
+from bokeh.transform import linear_cmap, log_cmap
+
 in_dir = "/sdf/home/r/richard/fermi-user/LSI61303/periods/periodicity/godot/diffrsp/fits/"
 
 # Get the list of files in the directory
@@ -20,7 +28,7 @@ spectrum = lambda E: (E/1000)**-2.1
 
 data = core.Data(ft1, ft2, ra, dec, weight_col="4FGL J0240.5+6113", base_spectrum=spectrum, zenith_cut=90)
 
-print("data input done")
+print("data prep done so far")
 
 ts = data.get_cells(tcell=300,time_series_only=True, trim_zero_exposure=False, use_barycenter=True)
 f,window = core.power_spectrum_fft(ts,exp_only=True)
@@ -28,8 +36,9 @@ scale = 50./40000
 f,dlogl_nobg,dlogl,dlogl_null = core.power_spectrum_fft(ts)
 fday = f*86400
 
-forb = 4.3684e-07
-fprec = 4.e-08
+#forb = 2.963145573933919e-06
+forb = 4.3676e-7
+fprec = 2.1777777777777778e-07
 freqs = np.asarray([fprec,forb,2*forb])
 corr,pows = core.get_orbital_modulation(ts,freqs)
 f2,dlogl_nobg2,dlogl2,dlogl_null2 = core.power_spectrum_fft(ts, exposure_correction=corr)
@@ -39,4 +48,11 @@ for freq,p in zip(freqs,pows):
     idx = np.argmin(np.abs(f[1:]-freq))
     add_power[idx] = p
 
-print("done so far")
+print("power spectrum done")
+
+fmask = fday < 0.1
+
+f1 = figure(title="frequency", yaxis_type="log", plot_width=800, plot_height=400)
+f1.line(fday[fmask], dlogl_nobg[fmask])
+
+save(f1)
